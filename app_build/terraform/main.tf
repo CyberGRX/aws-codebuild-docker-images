8388cef29308209760673b5f0e7d7ce80145e3ad
Charles Burton <charles.burton@cybergrx.com>
2019-07-31 09:06:41 -0600

    fit: initial release

---
+++ app_build/terraform/main.tf
@@ -0,0 +1,20 @@
+module "build_pipe" {
+  source          = "git::ssh://git@github.com/CyberGRX/terraform-aws-grx-codebuild.git?ref=v1.3.19"
+  region          = "${var.region}"
+  vpc_id          = "${lookup(data.terraform_remote_state.vpc.vpc_id,var.env)}"
+  private_subnets = ["${data.terraform_remote_state.vpc.private_subnets["${var.env}"]}"]
+  project_name    = "aws-codebuild-docker-images"
+  create_pipe     = "yes"
+  include_ecr     = "yes"
+  env             = "core"
+  build_image     = "aws/codebuild/standard:2.0"
+
+  parameter_list = [
+    "/build/secrets/daedalus",
+    "/codebuild/nexus/helm-password",
+  ]
+
+  providers = {
+    aws.core = "aws.core"
+  }
+}
