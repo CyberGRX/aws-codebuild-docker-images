b974e0b18f5844a283dc4a24382e035be1e97a35
Jake Gaylor <jake.gaylor@cybergrx.com>
2019-10-02 11:05:40 -0600

    bump buildpipe version to stop rotating out containers

--- app_build/terraform/main.tf
+++ app_build/terraform/main.tf
@@ -1,5 +1,5 @@
 module "build_pipe" {
-  source          = "git::ssh://git@github.com/CyberGRX/terraform-aws-grx-codebuild.git?ref=v1.3.19"
+  source          = "git::ssh://git@github.com/CyberGRX/terraform-aws-grx-codebuild.git?ref=v1.3.26"
   region          = "${var.region}"
   vpc_id          = "${lookup(data.terraform_remote_state.vpc.vpc_id,var.env)}"
   private_subnets = ["${data.terraform_remote_state.vpc.private_subnets["${var.env}"]}"]
@@ -8,6 +8,7 @@ module "build_pipe" {
   include_ecr     = "yes"
   env             = "core"
   build_image     = "aws/codebuild/standard:2.0"
+  github_repo_name    = "aws-codebuild-docker-images"
 
   parameter_list = [
     "/build/secrets/daedalus",
